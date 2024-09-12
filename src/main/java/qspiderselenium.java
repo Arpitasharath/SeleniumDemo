0fed9cb6c9285cc91e37da47922e2fdc703ca953
Arpitasharath <163119879+Arpitasharath@users.noreply.github.com>
2024-09-12 20:51:46 +0530

    Update qspiderselenium.java

--- src/main/java/qspiderselenium.java
+++ src/main/java/qspiderselenium.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class qspiderselenium {
-
+//pull back
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 	
